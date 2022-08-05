94da3a67f5fda238ec9e4c05985dcb3715410c3d
Ted Dino <ted23@tutanota.com>
2022-08-05 16:11:18 +0800

    add upload functionality

--- image-uploader/components/Uploaded.tsx
+++ image-uploader/components/Uploaded.tsx
@@ -1,5 +1,41 @@
+import { useEffect, useState } from "react";
+
 import Image from "next/image";
-const Uploaded = () => {
+import Link from "next/link";
+import { supabase } from "../utils/supabaseClient";
+
+const Uploaded = ({ fileName }: { fileName: string }) => {
+  const [link, setLink] = useState<string | undefined>("");
+  const [clipboard, setClipboard] = useState(false);
+
+  useEffect(() => {
+    const check = async () => {
+      try {
+        const { data, error } = supabase.storage
+          .from("images")
+          .getPublicUrl(fileName);
+        if (error) {
+          throw error;
+        }
+        setLink(data?.publicURL);
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log(error.message);
+        }
+        console.log("Unexpected Error", error);
+      }
+    };
+
+    check();
+  }, [fileName]);
+
+  const copyToClipboard = (path: string) => {
+    if (path !== undefined) {
+      navigator.clipboard.writeText(path);
+      setClipboard(true);
+    }
+  };
+
   return (
     <div className="w-[400px] h-[470px] bg-white shadow-xl rounded-xl flex flex-col justify-evenly items-center">
       <svg
@@ -14,19 +50,25 @@ const Uploaded = () => {
         />
       </svg>
       <h1 className="text-lg text-primary">Uploaded Successfully</h1>
-      <Image
-        className="rounded-xl"
-        src="/404.jpg"
-        width={338}
-        height={225}
-        alt="image not found"
-      />
+      <Link href={link ? link : ""}>
+        <a target="_blank" rel="noopener noreferrer">
+          <Image
+            className="rounded-xl object-contain"
+            src={link ? link : "/404.jpg"}
+            width={338}
+            height={225}
+            alt="image not found"
+          />
+        </a>
+      </Link>
+
       <div className="w-[338px] bg-gray-200 relative h-9 flex items-center rounded-xl p-1">
-        <small className="flex-1 text-left text-primary truncate">
-          https://image.com/sadfasdf/asdf/asdf/sadf/asd/sdfsdfsd/
-        </small>
-        <button className="px-5 bg-blue-500 h-[30px] text-white text-xs rounded-lg border border-blue-500 hover:bg-transparent hover:text-blue-500">
-          Copy Link
+        <small className="flex-1 text-left text-primary truncate">{link}</small>
+        <button
+          onClick={() => copyToClipboard(link!)}
+          className="px-5 bg-blue-500 h-[30px] text-white text-xs rounded-lg border border-blue-500 hover:bg-transparent hover:text-blue-500"
+        >
+          {clipboard ? "Copied" : "Copy Link"}
         </button>
       </div>
     </div>
--- image-uploader/pages/index.tsx
+++ image-uploader/pages/index.tsx
@@ -1,8 +1,36 @@
+import Error from "../components/Error";
 import Head from "next/head";
+import Loader from "../components/Loader";
 import type { NextPage } from "next";
 import Upload from "../components/Upload";
+import Uploaded from "../components/Uploaded";
+import { useState } from "react";
 
 const Home: NextPage = () => {
+  const [file, setFile] = useState<File | string>("");
+  const [fileName, setFileName] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<Error | null>(null);
+
+  const renderPage = () => {
+    if (error) {
+      return <Error message={`${error.message}`} />;
+    } else if (!file && loading) {
+      return (
+        <Upload
+          setFile={setFile}
+          setFileName={setFileName}
+          setLoading={setLoading}
+          setError={setError}
+        />
+      );
+    } else if (file && loading) {
+      return <Loader />;
+    } else if (!error && file && !loading) {
+      return <Uploaded fileName={fileName} />;
+    }
+  };
+
   return (
     <div className="flex min-h-screen flex-col items-center justify-center py-2">
       <Head>
@@ -15,7 +43,7 @@ const Home: NextPage = () => {
       </Head>
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
-        <Upload />
+        {renderPage()}
       </main>
     </div>
   );
